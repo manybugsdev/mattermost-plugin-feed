c8bbcd8c086c10578da42025eda62c585622b187
manybugsdev <me@manybugs.dev>
2025-04-10 20:54:15 +0900

    update bot constants and  add profile image

--- server/bot.go
+++ server/bot.go
@@ -1,17 +1,37 @@
 package main
 
-import "github.com/mattermost/mattermost/server/public/model"
+import (
+	"os"
+	"path"
 
-const BotName = "feedbot"
-const BotDisplayName = "Feed Bot"
-const BotDescription = "Feed Bot"
+	"github.com/mattermost/mattermost/server/public/model"
+)
+
+const BotName = "Feed"
+const BotDisplayName = "Feed"
+const BotDescription = "Bot for Feed Plugin"
 
 func (p *Plugin) EnsureFeedBot() (string, error) {
-	return p.client.Bot.EnsureBot(&model.Bot{
+	botId, err := p.client.Bot.EnsureBot(&model.Bot{
 		Username:    BotName,
 		DisplayName: BotDisplayName,
 		Description: BotDescription,
 	})
+	p.SetFeedBotProfileImage()
+	return botId, err
+}
+
+func (p *Plugin) SetFeedBotProfileImage() {
+	file, err := os.Open(path.Join("assets", "icon1024x1024.png"))
+	if err != nil {
+		p.client.Log.Error("Error opening profile image: " + err.Error())
+		return
+	}
+	defer file.Close()
+	err = p.client.User.SetProfileImage(p.botID, file)
+	if err != nil {
+		p.client.Log.Error("Error setting profile image: " + err.Error())
+	}
 }
 
 func (p *Plugin) BotPost(channelID string, text string) {
